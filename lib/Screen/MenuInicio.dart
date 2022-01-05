809ccb433bd1f04036406956dbb8a37fcb4ecb08
Samuel T <sammmmtts@gmail.com>
2022-01-05 13:57:19 -0800

    Inicio
    Inicio de la app, diseno de menu principal

---
+++ lib/Screen/MenuInicio.dart
@@ -0,0 +1,63 @@
+import 'package:appbar_example/main.dart';
+import 'package:flutter/material.dart';
+import 'package:line_awesome_flutter/line_awesome_flutter.dart';
+
+class SimpleAppBarPage extends StatefulWidget {
+  @override
+  _SimpleAppBarPageState createState() => _SimpleAppBarPageState();
+}
+
+class _SimpleAppBarPageState extends State<SimpleAppBarPage> {
+  @override
+  Widget build(BuildContext context) => DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(MyApp.title),
+            //centerTitle: true,
+            leading: IconButton(
+              icon: Icon(Icons.menu),
+              onPressed: () {},
+            ),
+            //backgroundColor: Colors.purple,
+            flexibleSpace: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Colors.indigo[600], Colors.lightBlueAccent],
+                  begin: Alignment.bottomRight,
+                  end: Alignment.topLeft,
+                ),
+              ),
+            ),
+            bottom: TabBar(
+              //isScrollable: true,
+              indicatorColor: Colors.white,
+              indicatorWeight: 5,
+              tabs: [
+                Tab(icon: Icon(LineAwesomeIcons.car), text: 'Vehiculo 1'),
+                Tab(icon: Icon(LineAwesomeIcons.car), text: 'Vehiculo 2'),
+                Tab(icon: Icon(LineAwesomeIcons.car), text: 'Vehiculo 3'),
+                Tab(icon: Icon(LineAwesomeIcons.car), text: 'Vehiculo 4'),
+              ],
+            ),
+            elevation: 20,
+            titleSpacing: 20,
+          ),
+          body: TabBarView(
+            children: [
+              buildPage('Home Page'),
+              buildPage('Feed Page'),
+              buildPage('Profile Page'),
+              buildPage('Settings Page'),
+            ],
+          ),
+        ),
+      );
+
+  Widget buildPage(String text) => Center(
+        child: Text(
+          text,
+          style: TextStyle(fontSize: 28),
+        ),
+      );
+}
---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+//import 'package:appbar_example/page/simple_appbar_page.dart';
+//import 'package:appbar_example/page/transparent_appbar_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'Screen/MenuInicio.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  static final String title = 'Car Gps';
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: title,
+        theme: ThemeData(
+          primaryColor: Colors.purple,
+        ),
+        home: MainPage(title: title),
+      );
+}
+
+class MainPage extends StatefulWidget {
+  final String title;
+
+  const MainPage({
+    @required this.title,
+  });
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int index = 1;
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        //bottomNavigationBar: buildBottomBar(),
+        body: buildPages(),
+      );
+
+  Widget buildBottomBar() {
+    final style = TextStyle(color: Colors.white);
+
+    return null;
+  }
+
+  Widget buildPages() {
+    switch (index) {
+      case 1:
+        return SimpleAppBarPage();
+      case 1:
+        return null;
+    }
+  }
+}
